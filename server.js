2c9566dd06e25df0c6b6fb7379e80b96a9c621c5
GedasPupa <gedosas@gmail.com>
2021-10-25 11:30:33 +0300

    express-validator

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mysql from "mysql";
 import cors from 'cors';
+import { body, check, validationResult  } from "express-validator";
 
 const port = 3000;
 const app = express();
@@ -15,7 +16,6 @@ const dbConfig = {
     password: "gedaspupa123",
     database: "scooters",
     multipleStatements: false,
-
 };
 
 const connection = mysql.createConnection({
@@ -33,93 +33,211 @@ connection.connect((error) => {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.get("/test-conn", (req, res) => {
+    connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
+        if (err) throw err;
+        console.log("The solution is: ", rows[0].solution);
+        res.status(200).send({ solution: rows[0].solution });
+    });
+});
 
-// app.get("/total", (req, res) => {
-//     connection.query("SELECT count(*) as total_records FROM books_table", (err, rows, fields) => {
-//         if (err) throw err;
-//         console.log("Total records: ", rows[0].total_records);
-//         res.status(200).send({ total_records: rows[0].total_records });
-//     });
-// });
-
-// app.get("/category/:cat", (req, res) => {
-//     connection.query("SELECT count(*) as category_count FROM books_table WHERE category = (?) GROUP BY category",
-//     req.params.cat,
-//     (err, rows, fields) => {
-//         if (err) throw err;
-//         console.log("Category count: ", rows[0].category_count);
-//         res.status(200).send({ category_count: rows[0].category_count });
-//     });    
-// });
-
-// GET all
+// GET all scooters:
 app.get("/scooters", (req, res) => {
     connection.query("SELECT * FROM paspirtukai", (err, rows, fields) => {
-        if (err) throw err;
+        if (err) {
+            console.log(err.message);
+            return res.status(500).send({
+                error_code: err.code,
+                error_message: err.sqlMessage,
+            });
+        };
+        try {
+            console.log('Nice! You got all', rows.length, 'scooters!');
+        } catch (err) {
+            console.log(err.message);
+        };
         res.status(200).send(rows);
     });
 });
-// GET one
+
+// GET one scooter:
 app.get("/scooters/:id", (req, res) => {
     connection.query(
         "SELECT * FROM paspirtukai WHERE id = ?",
         req.params.id,
         (err, rows, fields) => {
-            if (err) throw err;
+            // console.log({...fields});
+            if (err) {
+                console.log(err.message);
+                return res.status(500).send({
+                    error_code: err.code,
+                    error_message: err.sqlMessage,
+                });
+            };
+            try {
+                console.log('You got scooter with id: ', rows[0].id);
+            } catch (err) {
+                console.log(`Scooter with id ${req.params.id} not found!`);
+            };
+            if (rows.length === 0) {
+                return res.status(404).send({
+                    id: +req.params.id,
+                    error_message: 'Record not found'
+                });
+            }
             res.status(200).send(rows);
         }
     );
 });
-// CREATE one
-app.post("/scooters", (req, res) => {
-    connection.query(
-        "INSERT INTO paspirtukai (`name`, `busy`, `last_use`, `total_ride`) VALUES (?, ?, ?, ?)",
-        [
-            req.body.name,
-            req.body.busy,
-            req.body.last_use,
-            req.body.total_ride,
-        ],
-        (err, rows, field) => {
-            if (err) throw err;
-            console.log("created: ", { id: rows.insertId, ...req.body });
-            res.status(201).send({ id: rows.insertId, ...req.body });
-        }
-    );
-});
-// UPDATE one
-app.put("/scooters/:id", (req, res) => {
-    connection.query(
-        "UPDATE paspirtukai SET name = ?, busy = ?, last_use = ?, total_ride = ? WHERE id = ?",
-        [
-            req.body.name,
-            req.body.busy,
-            req.body.last_use,
-            req.body.total_ride,
-            req.params.id,
-        ],
-        (err, rows, field) => {
-            if (err) throw err;
-            console.log("updated: ", { rows });
-            res.status(201).send({id: parseInt(req.params.id), ...req.body});
-        }
-    ); 
-});
-// DELETE one
+
+// DELETE scooter:
 app.delete("/scooters/:id", (req, res) => {
     connection.query(
-        "DELETE FROM paspirtukai WHERE id=?",
+        "DELETE FROM paspirtukai WHERE id = ? ",
         req.params.id,
         (err, rows, field) => {
-            if (err) throw err;
-            console.log("deleted: ", rows);
-            res.status(204).send();
+            if (err) {
+                console.log(err.message);
+                return res.status(500).send({
+                    error_code: err.code,
+                    error_message: err.sqlMessage,
+                });
+            };
+            console.log("Deleted rows:", rows.affectedRows);
+            if (!rows.affectedRows) return res.status(404).send({
+                id: +req.params.id,
+                error_message: 'Record not found'
+            });
+            res.status(204).send({
+                id: +req.params.id,
+                message: `Record with id ${req.params.id} deleted`
+            });
         }
     );
+});
+
+// CREATE scooter:
+app.post(
+    "/scooters",
+
+    //validation:
+    check("name").isLength({min: 8, max: 8}).withMessage("Registration code must be 8 characters long!"),
+    check("busy").custom(value => (value == 0 || value == 1) ? true : false).withMessage("'busy' field must be 0 or 1!"),
+    check("last_use").isISO8601().toDate().withMessage("Not valid date format! Please enter: 'YYYY-MM-DD'."),
+    check("total_ride").custom(value => (value >= 0 && value <= 9999.99) ? true : false).withMessage("Min: 0 km, max: 9999.99 km!"),
+    
+    (req, res) => {
+        // validation:
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.errors[0].msg);
+            return res.status(400).json(errors);
+        }
+        // query to DB:
+        connection.query(
+            "INSERT INTO paspirtukai (name, busy, last_use, total_ride) VALUES (?, ?, ?, ?)",
+            [
+                req.body.name,
+                req.body.busy,
+                req.body.last_use,
+                req.body.total_ride,
+            ],
+            (err, rows, field) => {
+                if (err) {
+                    console.log(err.message);
+                    return res.status(500).send({
+                        error_code: err.code,
+                        error_message: err.sqlMessage,
+                    });
+                };
+                console.log("created: ", { id: rows.insertId, ...req.body });
+                res.status(201).send({ id: rows.insertId, ...req.body });
+            }
+        );
+    }
+);
+
+// UPDATE scooter:
+app.put(
+    "/scooters/:id",
+    
+    body('name').isLength({min: 8, max: 8}).withMessage("Registration code must be 8 characters long!"),
+
+    //validation:
+    check("name").isLength({min: 8, max: 8}).withMessage("Registration code must be 8 characters long!"),
+    check("busy").custom(value => (value == 0 || value == 1) ? true : false).withMessage("'busy' field must be 0 or 1!"),
+    check("last_use").isISO8601().toDate().withMessage("Not valid date format! Please enter: 'YYYY-MM-DD'."),
+    check("total_ride").custom(value => (value >= 0 && value <= 9999.99) ? true : false).withMessage("Min: 0 km, max: 9999.99 km!"),
+
+    (req, res) => {
+        // validation:
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.errors[0].msg);
+            return res.status(400).json(errors);
+        }
+        // query to DB:
+        connection.query(
+            "UPDATE paspirtukai SET name = ?, busy = ?, last_use = ?, total_ride = ? WHERE id = ?",
+            [
+                req.body.name,
+                req.body.busy,
+                req.body.last_use,
+                req.body.total_ride,
+                req.params.id,
+            ],
+            (err, rows, field) => {
+                if (err) {
+                    console.log(err.message);
+                    return res.status(500).send({
+                        error_code: err.code,
+                        error_message: err.sqlMessage,
+                    });
+                };
+                console.log("Updated rows:", rows === undefined ? 0 : rows.affectedRows);
+                if (!rows.affectedRows) {
+                    console.log(`Record with id ${req.params.id} not found!`);
+                    return res.status(404).send({
+                        id: +req.params.id,
+                        error_message: 'Record not found'
+                    });
+                }
+                res.status(201).send({id: +req.params.id, ...req.body});
+            }
+        ); 
+    }
+);
+
+// TOTAL scooters:
+app.get("/total", (req, res) => {
+    connection.query("SELECT count(*) as total_scooters FROM paspirtukai", (err, rows, fields) => {
+        if (err) {
+            console.log(err.message);
+            return res.status(500).send({
+                error_code: err.code,
+                error_message: err.sqlMessage,
+            });
+        };
+        console.log("Total scooters: ", rows[0].total_scooters);
+        res.status(200).send({ total_scooters: rows[0].total_scooters });
+    });
+});
+
+// TOTAL kilometers:
+app.get("/kilometers", (req, res) => {
+    connection.query("SELECT sum(total_ride) as total_kilometers FROM paspirtukai", (err, rows, fields) => {
+        if (err) {
+            console.log(err.message);
+            return res.status(500).send({
+                error_code: err.code,
+                error_message: err.sqlMessage,
+            });
+        };
+        console.log("Total kilometers: ", rows[0].total_kilometers);
+        res.status(200).send({ total_kilometers: rows[0].total_kilometers });
+    });
 });
 
 app.listen(port, () =>
     console.log(`Port: ${port}!`)
 );
-
-
